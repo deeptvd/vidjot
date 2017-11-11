68174587afb30271268f1ebc8bcdda748c8017f5
Deep Trivedi <deeptvd@gmail.com>
2017-11-11 18:41:13 -0500

    Added template engine and created views

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const exphbs  = require('express-handlebars'); 
 
 const app = express();
 
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars'); 
+
 //Index Route
 app.get('/', (req, res) => {
-  res.send('INDEX');
+  const title = "Welcome";
+  res.render('index', {
+    title:title
+  });
 });
 
 //About Route
 app.get('/about', (req, res) => {
-  res.send('Changes are detected');
+  res.render('about');
 });
 
 const port = 5000;
