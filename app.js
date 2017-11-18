65ae4c9f89eb4afcc216e9e764a26da4a88181ce
Deep Trivedi <deeptvd@gmail.com>
2017-11-17 21:34:42 -0500

    Add /ideas PUT

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars'); 
 const bodyParser = require('body-parser');
+var methodOverride = require('method-override')
 const mongoose = require('mongoose');
 
 const app = express();
@@ -24,8 +25,11 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars'); 
 
 //Body Parser middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Method override middleware
+app.use(methodOverride('_method'));
 
 //Index Route
 app.get('/', (req, res) => {
@@ -96,6 +100,22 @@ app.post('/ideas', (req, res) => {
         res.redirect('/ideas');
       });
   }
+});
+
+//Edit Form process
+app.put('/ideas/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  })
+    .then(idea => {
+      //new values 
+      idea.title = req.body.title;
+      idea.details = req.body.details;
+      idea.save()
+        .then(idea => {
+          res.redirect('/ideas');
+        });
+    });
 });
 
 const port = 5000;
